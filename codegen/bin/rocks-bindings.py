d0afe576a2896ce25a9a10f124074ddc316792f7
Andy Armstrong <andy@hexten.net>
2026-02-13 13:19:22 +0000

    Oof. Parsing.

--- codegen/bin/rocks-bindings.py
+++ codegen/bin/rocks-bindings.py
@@ -4,9 +4,10 @@
 #     "libclang>=18.1.1",
 # ]
 # ///
+from dataclasses import dataclass
+from functools import cached_property
 
-
-from clang.cindex import Cursor, CursorKind, Index, TypeKind
+from clang.cindex import Cursor, CursorKind, Index, Type, TypeKind
 
 HEADER = "../../rocksdb/include/rocksdb/c.h"
 
@@ -75,37 +76,72 @@ def cursor_name(cursor: Cursor) -> str:
         return "unknown"
 
 
-def with_cursor(cursor: Cursor, depth=0) -> None:
-    kind = cursor.kind
-    print(
-        "  " * depth
-        + cursor_kind_name(kind)
-        + " / "
-        + cursor_name(cursor)
-        + " / "
-        + cursor.type.kind.name,
-        end="",
-    )
-    type = cursor.type
-    if type.kind != TypeKind.INVALID:
-        print(" " + type.spelling, end="")
+DEBUG = False
 
-    match type.kind:
-        case TypeKind.FUNCTIONPROTO:
-            pass
+
+@dataclass(kw_only=True, frozen=True)
+class CursorWrapper:
+    cursor: Cursor
+
+    @cached_property
+    def name(self) -> str:
+        return self.cursor.spelling
+
+
+@dataclass(kw_only=True, frozen=True)
+class TypeWrapper:
+    type: Type
+
+    @cached_property
+    def name(self) -> str:
+        return self.type.spelling
+
+
+@dataclass(kw_only=True, frozen=True)
+class ArgWrapper(CursorWrapper):
+    @cached_property
+    def type(self) -> TypeWrapper:
+        return TypeWrapper(type=self.cursor.type)
+
+
+@dataclass(kw_only=True, frozen=True)
+class FunctionProtoWrapper(CursorWrapper):
+    @cached_property
+    def return_type(self) -> TypeWrapper:
+        return TypeWrapper(type=self.cursor.result_type)
+
+    @cached_property
+    def args(self) -> list[ArgWrapper]:
+        return [ArgWrapper(cursor=cursor) for cursor in self.cursor.get_arguments()]
+
+
+@dataclass(kw_only=True, frozen=True)
+class TypedefWrapper(CursorWrapper):
+    pass
+
+
+def scan_source(cursor: Cursor, depth=0) -> None:
+    match cursor.type.kind:
+        case TypeKind.FUNCTIONPROTO:  # ty:ignore[unresolved-attribute]
+            decl = FunctionProtoWrapper(cursor=cursor)
+            print(f"function: {decl.name}")
+            print(f"  return: {decl.return_type.name}")
+            for arg in decl.args:
+                print(f"     arg: {arg.type.name} {arg.name}")
+            print()
+        case TypeKind.TYPEDEF:
+            decl = TypedefWrapper(cursor=cursor)
+            print(f"typedef {decl.name}")
+            print()
         case _:
-            pass
-
-    print(" " + cursor.spelling)
-
-    for c in cursor.get_children():
-        with_cursor(c, depth + 1)
+            for c in cursor.get_children():
+                scan_source(c, depth + 1)
 
 
 def main(header: str) -> None:
     idx = Index.create()
     tu = idx.parse(header)
-    with_cursor(tu.cursor)
+    scan_source(tu.cursor)
 
 
 if __name__ == "__main__":
