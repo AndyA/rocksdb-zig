04eba3c0ccb811456f87d8653622f2417c082402
Andy Armstrong <andy@hexten.net>
2026-02-26 14:51:59 +0000

    Zig experiments

--- auto-bindings/src/mule.zig
+++ auto-bindings/src/mule.zig
@@ -1,8 +1,15 @@
 const std = @import("std");
 const Io = std.Io;
+const print = std.debug.print;
 
 pub fn main(_: std.process.Init) !void {
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
 }
 
-test "simple test" {}
+test {
+    var root: u32 = 1;
+    const handle: [*c]u32 = @ptrCast(&root);
+    const wrapper: packed struct { ref: *u32 } = @bitCast(@intFromPtr(handle));
+
+    try std.testing.expectEqual(handle, @as([*c]u32, @ptrFromInt(@as(usize, @bitCast(wrapper)))));
+}
